1aa5c23abc90a4a5c3e86b19f7cd9904501ef070
Anup Ghosh <anupbsmrstu@gmail.com>
2023-05-18 11:58:51 +0600

    Cpp

---
+++ 1820 problem A.cpp
@@ -0,0 +1,47 @@
+// Problem: A. Yura's New Name
+// Contest: Codeforces - Codeforces Round 866 (Div. 2)
+// URL: https://codeforces.com/contest 1820 problem A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+/*//*Anup Ghosh*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pi acos(-1)
+#define ull unsigned long long
+#define nl printf("\n")
+#define MAXN 1000005
+#define gcd(a, b) __gcd(a, b)
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define fio()                   \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+#define Erase(s) s.erase(unique(s.begin(), s.end()), s.end())
+
+#define sp(n) fixed << setprecision(n)
+#define mod 998244353
+#define no cout << "NO\n";
+
+#define yes cout << "YES\n";
+
+void solve(int tc) {}
+
+int main() {
+  int t;
+  t = 1;
+  cin >> t;
+  int x = 1;
+  while (t--) {
+    solve(x);
+    x++;
+  }
+
+  return 0;
+}
